8659d3d3a5733100cbe72a07ad05b0480f40d5cb
Tendsin Mende <tendsin@protonmail.com>
2023-08-17 15:06:13 +0200

    first AST draft

--- crates/vola-ast/src/lib.rs
+++ crates/vola-ast/src/lib.rs
@@ -4,7 +4,22 @@
 //!
 //!
 //! The AST is designed to be easily transformed into SSA 3-address code. This allows us to transform it into
-//! MLIR Code, or directly into SPIR-V, depending on the choosen back/middle end.
+//! MLIR Code, or directly into SPIR-V, depending on the chosen back/middle end.
+//!
+//!
+//! The design somewhat reflects the design of the vola language syntax. There are two levels,
+//! a `cobinatorical`, which applies operations to primitives, and, at the top level defines a `field`.
+//!
+//! Each of those primitives (and arguments to operations) are defined by an algebraic expression. That could be
+//! a simple value, lets say `5`, or an expression, lets say `5+a`.
+//!
+//! The AST thefore is split into two as well. The toplevel `combinatorical`, and a lower level, more traditional
+//! algebraic AST. For simplicity we call the combinatorical AST just **AST**.
+//!
+//! This allows a compiler to first resolve the evaluation time of each algebraic expression, before compiling down to
+//! the actual equation.
+//!
+//! NOTE(tendsin): Lets see if this actually works out this way :D
 
 use std::path::Path;
 
@@ -23,17 +38,156 @@ pub enum AstError {
     ParseError,
 }
 
-///All node types
-pub enum Node {
-    EntryPoint,
+///Type description
+pub enum Ty {
+    Scalar,
+    Vector { count: usize },
+    Mat { width: usize, height: usize },
 }
 
-pub struct Ast {}
+//a string identifier
+pub struct Identifier(String);
+//A single digit
+pub struct ImmVal(usize);
+//a float immediate value
+pub struct ImmFloat(ImmVal, ImmVal);
+
+pub struct TypedIdent {
+    ident: Identifier,
+    ty: Option<Ty>,
+}
+
+pub enum UnOp {
+    //The ! op
+    Not,
+    // The - op
+    Neg,
+}
+
+pub enum BinOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+}
+
+///Algebraic expression.
+pub struct AlgeExpr {}
+
+///Let statements are our entry point from the
+/// combinatorical level to the algebraic
+pub struct LetStmt {
+    identifier: TypedIdent,
+    stmt: AlgeExpr,
+}
+
+pub struct AlgeAst {
+    identifier: Identifier,
+    args: Vec<TypedIdent>,
+    stmts: Vec<LetStmt>,
+    ret: AlgeExpr,
+}
+
+pub enum PrimStmt {
+    PrimDef(Identifier),
+    LetStmt(LetStmt),
+    PrimAssignment {
+        prim: Identifier,
+        field: Identifier,
+        expr: AlgeExpr,
+    },
+}
+
+pub struct PrimAst {
+    identifier: Identifier,
+    args: Vec<TypedIdent>,
+    stmts: Vec<PrimStmt>,
+    //Always needs to return a identifier to a primitive
+    ret: Identifier,
+}
+
+pub enum Assignment {
+    FieldAssignment {
+        prim: Identifier,
+        field: Identifier,
+        ///Possible binary operation on former value
+        op: Option<BinOp>,
+        expr: AlgeExpr,
+    },
+    AtAssignment {
+        op: Option<BinOp>,
+        expr: AlgeExpr,
+    },
+}
+
+pub enum OpStmt {
+    PrimDef {
+        identifier: Identifier,
+        assignment: Option<OpTree>,
+    },
+    LetStmt(LetStmt),
+    Assignment(Assignment),
+}
+
+pub struct OpAst {
+    identifier: Identifier,
+    prims: Vec<Identifier>,
+    args: Vec<TypedIdent>,
+    stmts: Vec<OpStmt>,
+    ret: OpTree,
+}
+
+pub enum OpTree {
+    Ident(Identifier),
+    ///Calls into a primitive definition
+    PrimCall {
+        ident: Identifier,
+        args: Vec<AlgeExpr>,
+    },
+    Op {
+        op_ident: Identifier,
+        sub_trees: Vec<OpTree>,
+        args: Vec<AlgeExpr>,
+    },
+}
+
+pub enum FieldStmt {
+    PrimDef {
+        identifier: Identifier,
+        op_tree: OpTree,
+    },
+    LetStmt(LetStmt),
+}
+
+/// A field is defined as a set of statements, where each is either an arithmetic
+/// let expression, a primitive definition or a so called Op-Tree (tree of OPs)
+///
+/// In addition the field must return an op tree (which is used to build the final evaluation order).
+pub struct FieldAst {
+    identifier: Identifier,
+    args: Vec<TypedIdent>,
+    ///Order of statements
+    statments: Vec<FieldStmt>,
+    return_tree: OpTree,
+}
+
+pub struct Ast {
+    fields: Vec<FieldAst>,
+    ops: Vec<OpAst>,
+    prims: Vec<PrimAst>,
+    alges: Vec<AlgeAst>,
+}
 
 impl Ast {
     ///Creates an empty Ast with the given root node
-    fn empty(root: Node) -> Self {
-        Ast {}
+    fn empty() -> Self {
+        Ast {
+            fields: Vec::new(),
+            ops: Vec::new(),
+            prims: Vec::new(),
+            alges: Vec::new(),
+        }
     }
 
     ///Tries to parse `file` using `tree-sitter-vola` into an [Ast].
@@ -44,7 +198,7 @@ impl Ast {
 
         //TODO transform to ast
 
-        let ast = Ast {};
+        let mut ast = Ast::empty();
 
         Ok(ast)
     }
