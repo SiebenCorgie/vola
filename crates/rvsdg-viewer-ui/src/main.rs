410e2047d8bab690ccd9c7f7f869727e4ba0b330
Tendsin Mende <tendsin@protonmail.com>
2024-04-23 16:42:28 +0200

    Iced can now do everything the macroquad viewer could

--- crates/rvsdg-viewer-ui/src/main.rs
+++ crates/rvsdg-viewer-ui/src/main.rs
@@ -1,4 +1,5 @@
 use graph_canvas::{GraphCanvas, GraphCanvasMessage};
+use iced::widget::canvas::path::lyon_path::geom::euclid::num::Round;
 /*
 use core::panic;
 use std::path::Path;
@@ -35,8 +36,10 @@ fn main() {
     };
 }
 */
-use iced::widget::{button, column, text};
-use iced::{Alignment, Element, Length, Sandbox, Settings};
+use iced::widget::{
+    button, column, container, row, scrollable, slider, text, vertical_space, Column, Scrollable,
+};
+use iced::{Alignment, Element, Length, Sandbox, Settings, Theme};
 use rvsdg::attrib::AttribLocation;
 use rvsdg_viewer::ViewerState;
 
@@ -54,18 +57,24 @@ struct Ui {
     ///Attrib that is currently selected
     selected: Option<AttribLocation>,
     viewer: ViewerState,
+    selected_state: usize,
     graph_canvas: GraphCanvas,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     Select(Option<AttribLocation>),
+    TimelineSelect(usize),
     ResetCanvas,
     Dummy,
 }
 
 impl Sandbox for Ui {
     type Message = Message;
+
+    fn theme(&self) -> Theme {
+        Theme::GruvboxDark
+    }
 
     fn new() -> Self {
         //read states from command line
@@ -87,6 +96,7 @@ impl Sandbox for Ui {
         Ui {
             selected: None,
             viewer,
+            selected_state: 0,
             graph_canvas: GraphCanvas::new(),
         }
     }
@@ -99,25 +109,72 @@ impl Sandbox for Ui {
         match message {
             Message::ResetCanvas => self.graph_canvas.reques_redraw(),
             Message::Select(t) => {
-                println!("Select override!");
-                self.selected = t
+                self.selected = t;
+            }
+            Message::TimelineSelect(s) => {
+                self.selected_state = s;
             }
             _ => println!("Au bagge!"),
         }
     }
 
     fn view(&self) -> Element<Message> {
-        column![
-            text("This would be a graph name")
-                .width(Length::Shrink)
-                .size(50),
-            self.graph_canvas.view(&self.viewer.states[0]).map(|msg| {
-                match msg {
-                    GraphCanvasMessage::Redraw => Message::ResetCanvas,
-                    GraphCanvasMessage::Select(s) => Message::Select(s),
+        //Our ui works by positioning
+
+        let timeline_slider = slider(
+            0u8..=(self.viewer.states.len() - 1) as u8,
+            self.selected_state as u8,
+            |changed| Message::TimelineSelect(changed as usize),
+        );
+
+        let properties: Element<Message> = if let Some(selected) = &self.selected {
+            if let Some(properties) = self.viewer.states[self.selected_state]
+                .auxilary_data
+                .get(selected)
+            {
+                let mut items = Column::new();
+                for (pname, props) in properties {
+                    items = items.push(
+                        container(
+                            row![text(pname.clone()), {
+                                let mut plist = Column::new();
+                                for p in props {
+                                    plist = plist.push(text(p.clone()));
+                                }
+
+                                plist.padding(2.5)
+                            }]
+                            .spacing(5.0),
+                        )
+                        .padding(10.0),
+                    );
                 }
-            }),
-            button("Clear").padding(8).on_press(Message::Dummy),
+
+                Scrollable::new(items).into()
+            } else {
+                text("No auxilary data").into()
+            }
+        } else {
+            text("Nothing selected!").into()
+        };
+        let sidepanel = column![text("Properties").size(20.0), properties];
+
+        column![
+            text(&self.viewer.states[self.selected_state].name)
+                .width(Length::Shrink)
+                .size(20),
+            row![
+                column![
+                    self.graph_canvas.view(&self.viewer.states[0]).map(|msg| {
+                        match msg {
+                            GraphCanvasMessage::Redraw => Message::ResetCanvas,
+                            GraphCanvasMessage::Select(s) => Message::Select(s),
+                        }
+                    }),
+                    column![text("Timeline"), timeline_slider]
+                ],
+                sidepanel.padding(20.0).max_width(400.0)
+            ]
         ]
         .padding(20)
         .spacing(20)
