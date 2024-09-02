1246c3da076ec98bcfe041f44ecd683567d1b112
Tendsin Mende <tendsin@protonmail.com>
2024-09-02 13:01:20 +0200

    add simple AD tests

--- crates/volac/tests/autodiff.rs
+++ crates/volac/tests/autodiff.rs
@@ -7,3 +7,30 @@ fn ad_sphere() {
         .unwrap();
     target.try_verify().unwrap();
 }
+
+#[test]
+fn ad_sphere_vec() {
+    let pipeline = volac::Pipeline::new_in_memory();
+    let target = pipeline
+        .execute_on_file(&"tests/vola_src/autodiff_sphere_vec.vola")
+        .unwrap();
+    target.try_verify().unwrap();
+}
+
+#[test]
+fn ad_sphere_with_external() {
+    let pipeline = volac::Pipeline::new_in_memory();
+    let target = pipeline
+        .execute_on_file(&"tests/vola_src/autodiff_sphere_with_external.vola")
+        .unwrap();
+    target.try_verify().unwrap();
+}
+
+#[test]
+fn ad_sphere_with_external_vec() {
+    let pipeline = volac::Pipeline::new_in_memory();
+    let target = pipeline
+        .execute_on_file(&"tests/vola_src/autodiff_sphere_with_external_vec.vola")
+        .unwrap();
+    target.try_verify().unwrap();
+}
