6c9d1c91f174099c7a70408f479dfaa1ed1e6c62
Tendsin Mende <tendsin@protonmail.com>
2025-03-28 15:26:57 +0100

    do not include dot-feature in vola-ast, since that (apparently) includes it in any _use_ of vola-ast

--- crates/vola-ast/examples/simple_csg.rs
+++ crates/vola-ast/examples/simple_csg.rs
@@ -1,5 +1,3 @@
-use vola_ast::dot::ast_to_svg;
-
 pub fn main() {
     let parser = vola_tree_sitter_parser::VolaTreeSitterParser;
     let ast = match vola_ast::VolaAst::new_from_file(&"crates/vola-ast/examples/csg.vola", &parser)
@@ -10,6 +8,6 @@ pub fn main() {
             return;
         }
     };
-
-    ast_to_svg(&ast, "simple_csg.svg");
+    //NOTE: pass --feature "dot" to enable printing to ast
+    //vola_ast::dot::ast_to_svg(&ast, "simple_csg.svg");
 }
