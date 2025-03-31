5ea5aa59f0a24ee4a16f6d3a81bdded43e250f46
Tendsin Mende <tendsin@protonmail.com>
2025-03-31 18:20:32 +0200

    let AST use new VolaError, and collect _all_ errors, instead of bailing on the first one in a module

--- crates/vola-ast/src/error.rs
+++ crates/vola-ast/src/error.rs
@@ -5,15 +5,16 @@
  *
  * 2024 Tendsin Mende
  */
-use std::path::PathBuf;
+use std::{error::Error, path::PathBuf};
 
 use thiserror::Error;
+use vola_common::VolaError;
 
 ///Errors that happen while working on the AST.
 #[derive(Debug, Error, Clone, PartialEq)]
 pub enum AstError {
-    #[error("Failed to parse file {path:?}: {err}")]
-    ParsingError { path: String, err: String },
+    #[error("Failed to parse file: {0}")]
+    ParsingError(String),
 
     #[error("Not parsing from file. Therfore no sub-module can be used.")]
     NoRootFile,
@@ -21,4 +22,15 @@ pub enum AstError {
     NoModuleFile { path: PathBuf },
     #[error("IO Error: {0}")]
     IoError(String),
+}
+
+impl AstError {
+    pub fn from_parser_error<E: Error>(error: VolaError<E>) -> VolaError<Self> {
+        let internal = Self::ParsingError(format!("{}", error.error.to_string()));
+        VolaError {
+            error: internal,
+            source_span: error.source_span,
+            labels: error.labels,
+        }
+    }
 }
--- crates/vola-ast/src/passes.rs
+++ crates/vola-ast/src/passes.rs
@@ -1,20 +1,27 @@
-use std::path::Path;
+use std::{error::Error, path::Path};
 
 use smallvec::SmallVec;
-use vola_common::FileString;
+use vola_common::{FileString, VolaError};
 
 use crate::{common::Ident, error::AstError, AstEntry, VolaAst, VolaParser};
 
 impl VolaAst {
-    pub(crate) fn resolve_module(
+    pub(crate) fn resolve_module<E: Error>(
         file: &dyn AsRef<Path>,
         stem_path: &[Ident],
-        parser: &dyn VolaParser,
-    ) -> Result<VolaAst, AstError> {
+        parser: &dyn VolaParser<Error = E>,
+    ) -> Result<VolaAst, Vec<VolaError<AstError>>> {
         let file_name: FileString = file.as_ref().to_str().unwrap().into();
 
-        let bytes = std::fs::read(file.as_ref()).map_err(|e| AstError::IoError(e.to_string()))?;
-        let mut parsed = parser.parse_from_byte(Some(file_name), &bytes)?;
+        let bytes = std::fs::read(file.as_ref())
+            .map_err(|e| vec![VolaError::new(AstError::IoError(e.to_string()))])?;
+        let mut parsed = parser
+            .parse_from_byte(Some(file_name), &bytes)
+            .map_err(|errs| {
+                errs.into_iter()
+                    .map(|e| AstError::from_parser_error(e))
+                    .collect::<Vec<_>>()
+            })?;
 
         //prepend the stem_path to all submodules that are imported
         for tl in parsed.entries.iter_mut() {
