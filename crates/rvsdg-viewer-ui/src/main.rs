f8293cfd5743dde9384784cb2d9c68c58d8588b9
Tendsin Mende <tendsin@protonmail.com>
2025-05-08 17:21:46 +0200

    update viewer to iced 0.13, and allow free selection of items by typing ID

--- crates/rvsdg-viewer-ui/src/main.rs
+++ crates/rvsdg-viewer-ui/src/main.rs
@@ -10,7 +10,7 @@
 
 use graph_canvas::{GraphCanvas, GraphCanvasMessage};
 use iced::widget::{column, container, row, slider, text, Column, Rule, Scrollable, Toggler};
-use iced::{Alignment, Element, Length, Sandbox, Settings, Theme};
+use iced::{Alignment, Element, Length, Task, Theme};
 use rvsdg::attrib::AttribLocation;
 use rvsdg_viewer::ViewerState;
 
@@ -18,10 +18,9 @@ pub(crate) mod collide;
 mod graph_canvas;
 
 pub fn main() -> iced::Result {
-    Ui::run(Settings {
-        antialiasing: true,
-        ..Settings::default()
-    })
+    iced::application("RVSDG-Ui", Ui::update, Ui::view)
+        .antialiasing(true)
+        .run()
 }
 
 struct Ui {
@@ -29,6 +28,7 @@ struct Ui {
     selected: Option<AttribLocation>,
     viewer: ViewerState,
     selected_state: usize,
+    selection_string: String,
     graph_canvas: GraphCanvas,
     is_dark: bool,
 }
@@ -39,14 +39,12 @@ pub enum Message {
     TimelineSelect(usize),
     ResetCanvas,
     ToggleTheme(bool),
-
+    SelectionStringChanged(String),
     Dummy,
 }
 
-impl Sandbox for Ui {
-    type Message = Message;
-
-    fn new() -> Self {
+impl Default for Ui {
+    fn default() -> Self {
         //read states from command line this is some strange text.
 
         let path = if let Some(p) = std::env::args().skip(1).next() {
@@ -68,23 +66,14 @@ impl Sandbox for Ui {
             viewer,
             selected_state: 0,
             graph_canvas: GraphCanvas::new(),
+            selection_string: String::new(),
             is_dark: false,
         }
     }
+}
 
-    fn theme(&self) -> Theme {
-        if self.is_dark {
-            Theme::SolarizedDark
-        } else {
-            Theme::SolarizedLight
-        }
-    }
-
-    fn title(&self) -> String {
-        String::from("RVSDG - Viewer")
-    }
-
-    fn update(&mut self, message: Message) {
+impl Ui {
+    fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::ResetCanvas => self.graph_canvas.reques_redraw(),
             Message::Select(t) => {
@@ -95,8 +84,20 @@ impl Sandbox for Ui {
                 self.graph_canvas.reques_redraw();
             }
             Message::ToggleTheme(pl) => self.is_dark = pl,
-            _ => println!("Au bagge!"),
+            Message::SelectionStringChanged(new) => {
+                self.selection_string = new;
+                if let Ok(new_attrib_location) = self.selection_string.parse::<AttribLocation>() {
+                    //update internal graph
+                    *self.graph_canvas.selection.lock().unwrap() =
+                        Some(new_attrib_location.clone());
+                    self.graph_canvas.reques_redraw();
+                    return Task::done(Message::Select(Some(new_attrib_location)));
+                }
+            }
+            other => println!("Unhandeled message: {other:?}"),
         }
+
+        Task::none()
     }
 
     fn view(&self) -> Element<Message> {
@@ -121,7 +122,7 @@ impl Sandbox for Ui {
                 let mut items = Column::new();
                 //always prepend the attrib name
                 items = items
-                    .push(container(text(&format!("{}", selected)).size(20.0)).padding(10.0))
+                    .push(container(text(format!("{}", selected)).size(20.0)).padding(10.0))
                     .push(Rule::horizontal(1.0));
 
                 for (pname, props) in properties {
@@ -144,12 +145,30 @@ impl Sandbox for Ui {
 
                 Scrollable::new(items).into()
             } else {
-                text(&format!("{}", selected)).size(20.0).into()
+                text(format!("{}", selected)).size(20.0).into()
             }
         } else {
             text("Nothing selected!").into()
         };
         let sidepanel = column![text("Properties").size(30.0), properties];
+
+        //Try to parse the string, if okay, use _success_ styling
+        let is_parsing = self.selection_string.parse::<AttribLocation>().is_ok();
+        let is_empty = self.selection_string.is_empty();
+        let selection_string_styling = move |t: &Theme, s: _| {
+            let mut default_style = iced::widget::text_input::default(t, s);
+            if is_empty {
+                default_style
+            } else {
+                if is_parsing {
+                    default_style.border.color = t.extended_palette().success.strong.color;
+                    default_style
+                } else {
+                    default_style.border.color = t.extended_palette().danger.strong.color;
+                    default_style
+                }
+            }
+        };
 
         column![
             text(&self.viewer.states[self.selected_state].name)
@@ -159,11 +178,15 @@ impl Sandbox for Ui {
             column![
                 text("Timeline"),
                 timeline_slider,
-                row![Toggler::new(
-                    "Toggle dark mode".to_owned(),
-                    self.is_dark,
-                    |state| { Message::ToggleTheme(state) }
-                ),],
+                row![
+                    Toggler::new(self.is_dark)
+                        .label("Toggle dark mode".to_owned())
+                        .on_toggle(Message::ToggleTheme),
+                    iced::widget::text_input("TypeSelection", &self.selection_string)
+                        .style(selection_string_styling)
+                        .on_input(|input| Message::SelectionStringChanged(input))
+                ]
+                .spacing(20.0),
             ]
             .spacing(10.0),
             Rule::horizontal(2.0),
@@ -185,7 +208,7 @@ impl Sandbox for Ui {
         ]
         .padding(20)
         .spacing(20)
-        .align_items(Alignment::Center)
+        .align_x(Alignment::Center)
         .into()
     }
 }
