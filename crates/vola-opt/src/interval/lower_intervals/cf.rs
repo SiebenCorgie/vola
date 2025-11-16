34f7b640a6095e2c73226c93aa816a5a7597f9b5
Sieben <tendsin@protonmail.com>
2025-11-16 10:21:28 +0100

    theta interface handling for interval lowering

--- crates/vola-opt/src/interval/lower_intervals/cf.rs
+++ crates/vola-opt/src/interval/lower_intervals/cf.rs
@@ -6,8 +6,12 @@
  * 2025 Tendsin Mende
  */
 
-use rvsdg::{region::RegionLocation, NodeRef};
-use vola_common::{Span, VolaError};
+use rvsdg::{
+    edge::{InputType, OutputType},
+    region::RegionLocation,
+    NodeRef,
+};
+use vola_common::VolaError;
 
 use crate::{interval::lower_intervals::LowerIntervals, OptError};
 
@@ -20,7 +24,49 @@ impl<'opt> LowerIntervals<'opt> {
         // and disassembling the loop-arguments/loop-results via indexing and recording those mappings.
         //
         // After that we'd rewrite the port's types as well as the edge-types.
-        todo!()
+
+        let loop_body = RegionLocation {
+            node,
+            region_index: 0,
+        };
+
+        //Regardless of early-exit or not, push the loop body
+        self.region_queue.push_front(loop_body);
+
+        let parent_region = self.optimizer.graph[node].parent.unwrap();
+
+        //now go through each lv and split/assemble them if they are interval typed
+        for lv in 0..self.optimizer.graph[node]
+            .node_type
+            .unwrap_theta_ref()
+            .loop_variable_count()
+        {
+            if !self
+                .optimizer
+                .find_type(node.input(lv))
+                .unwrap()
+                .is_interval()
+            {
+                continue;
+            }
+
+            //unwrap the lv-input...
+            self.itt_inport(parent_region, node.input(lv));
+            //... and the associated lv-arg
+            self.itt_outport(
+                loop_body,
+                node.as_outport_location(OutputType::Argument(lv)),
+            );
+            //now do the same for the result...
+            self.itt_inport(loop_body, node.as_inport_location(InputType::Result(lv)));
+            //... and its associated output
+            self.itt_outport(
+                parent_region,
+                node.as_outport_location(OutputType::Output(lv)),
+            );
+        }
+
+        Ok(())
     }
 
     pub(crate) fn lower_gamma(&mut self, node: NodeRef) -> Result<(), VolaError<OptError>> {
