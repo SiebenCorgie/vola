8c90064a0beb83a68ecbbbbdd51862bab4d7492e
Tendsin Mende <tendsin@protonmail.com>
2026-04-06 18:33:47 +0200

    add raytracing example

---
+++ examples/raytracing/src/main.rs
@@ -0,0 +1,6 @@
+//! A simple softbuffer based CPU raytracer that utilizes Vola's bridge helper to update the ray-traced scene
+//! whenever `scene.vola` changes.
+
+fn main() {
+    println!("Hello ray-tracer!");
+}
